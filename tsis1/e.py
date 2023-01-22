6a7bacb3d1ce3e569f1fb6efb1e356fc9c7d17d7
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2023-01-22 22:31:21 +0600

    Add files via upload

---
+++ tsis1/e.py
@@ -0,0 +1,31 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+#//////
+
+x = 4 
+x = "Sally"
+print(x)
+
+#/////
+x = str(3)
+y = int(3)
+z = float(3)
+
+#//////
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#//////
+x = "John"
+# is the same as
+x = 'John'
+
+#////////
+a = 4
+A = "Sally"
+#A will not overwrite a
